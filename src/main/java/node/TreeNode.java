b8b21023d889561bb694c6e61b1127d3363cdbde
Guolong Wang <13121593633@163.com>
2021-01-28 15:59:32 +0800

    feat: 添加二叉树的节点类

---
+++ src/main/java/node/TreeNode.java
@@ -0,0 +1,17 @@
+package node;
+
+/**
+ * 二叉树的节点类
+ *
+ * @author mango
+ * @create 2019-03-17 2:17 PM
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
