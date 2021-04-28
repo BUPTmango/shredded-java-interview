cd6c9956ca9618f8f4b794a75643b17ae91f9b1b
Guolong Wang <13121593633@163.com>
2021-04-28 22:38:11 +0800

    class

---
+++ redisson-spring-boot-starter/src/test/java/com/wgl/redisson/RedissonSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgl.redisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
