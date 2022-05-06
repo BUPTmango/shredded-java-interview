c4e9f8268d5360c3792fe2300585c9a63e2e30fe
Guolong Wang <13121593633@163.com>
2022-05-06 13:16:01 +0800

    feat: 手撕spring statemachine下单场景

--- src/main/java/spring/statemachine/ApplicationStarter.java
+++ src/main/java/spring/statemachine/ApplicationStarter.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
+ * 1 定义状态和事件的枚举类
+ * 2 定义状态机配置（设置初始状态以及状态与事件之间的关系）
+ * 3 定义状态监听器 （当状态变更时，触发方法）
+ *
  * @author Wang Guolong
  * @version 1.0
  * @date 2022/5/6 12:59
